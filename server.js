7591670165a06f4abaf85d3d33f69d60b6c44e36
husmannm <maria.husmann@inf.ethz.ch>
2016-03-04 11:49:15 +0100

    demo

--- server.js
+++ server.js
@@ -18,14 +18,59 @@
  * See the README and LICENSE files for further information.
  *
  */
+
+var XDmvcServer = require('xdmvcserver/xdmvcserver.js');
 var path = require('path');
+var xdmvc = new XDmvcServer();
 
 var connect = require('connect'),
-    http = require('http'),
+	http = require('http'),
+	bodyParser = require('body-parser'),
 	serveStatic = require('serve-static');
-var app = connect().use(serveStatic(path.join(__dirname, 'public')));
+
+var url = require('url');
+
+var app = connect().use(bodyParser.json()).use(serveStatic(path.join(__dirname, 'public')));
+app.use("/gallery", handleGallery);
+
 var server = http.createServer(app);
+var fs = require('fs');
+var basePath = path.join('public', 'img', 'album');
+var album = {};
+
+function createModel() {
+//	var folder = fs.readdirSync(basePath)
+
+console.log(basePath);
+//	thumbs = fs.readdirSync(path.join(basePath, folder, 'thumbs'));
+	var files = fs.readdirSync(path.join(basePath, 'thumbs'));
+	var largePath = "img/album/large/";
+	var thumbsPath = "img/album/thumbs/";
+	// Read images from directory
+	album.thumbs = files.map(function (f) {
+		return thumbsPath + f;
+	});
+	album.large = files.map(function (f) {
+		return largePath + f;
+	});
+
+	console.log(album);
+}
+function handleGallery(req, res, next){
+	var parsedUrl = url.parse(req.url, true)
+	var query = parsedUrl.query;
+	var path = parsedUrl.pathname.split("/");
+	res.setHeader('Access-Control-Allow-Origin', '*')
+
+	if (path.length === 2 && path[1].length ===0){
+		res.writeHead(200, {'Content-Type': 'text/json' });
+		res.write(JSON.stringify(album));
+		res.end('\n');
+	}
+
+}
+
+createModel();
+xdmvc.start();
 
 server.listen(8082);
-
-
